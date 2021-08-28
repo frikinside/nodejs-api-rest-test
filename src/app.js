717389ab0a257e1b6a9ad16c89a2b3cf8466c11d
frikinside <friki.inside@gmail.com>
2021-08-28 20:16:21 +0200

    added request logger

--- src/app.js
+++ src/app.js
@@ -11,6 +11,19 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(methodOverride());
 
+app.use((req, res, next) => {
+	var req_data = {
+		method: req.method,
+		path: req.url,
+		ip: req.headers['x-forwarded-for'] || req.socket.remoteAddress,
+		start_date: new Date(),
+	};
+	console.log(`${req_data.start_date.toISOString()} Started request ${req_data.method} ${req_data.path} from ${req_data.ip}`);
+	next();
+	req_data.finish_date = new Date();
+	console.log(`${req_data.finish_date.toISOString()} Finished request ${req_data.method} ${req_data.path} from ${req_data.ip} in ${req_data.finish_date - req_data.start_date} ms`);
+});
+
 const routes = require('./routes');
 app.use(routes);
 app.use((err, req, res, next) => {
